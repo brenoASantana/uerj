cebb3dcb5f0bdf7f8018b28782aec0996034f7ea
Breno Santana <contatobrenosantana@outlook.com>
2024-03-21 00:32:15 -0300

    Exercício Corrigido

--- exerc_aed1/Feitos em sala/240319/atividade.c
+++ exerc_aed1/Feitos em sala/240319/atividade.c
@@ -1,4 +1,4 @@
-// Feito por Breno Alexandre Santana Silva e Matheus da Silva de Oliveira
+//Feito por Breno Alexandre Santana Silva e Matheus da Silva de Oliveira
 #include <stdio.h>
 #define MAX 20
 
@@ -12,13 +12,12 @@ typedef struct professor
 
 void leitura(professor *prof, int *numProf)
 {
-
     printf("Digite a matricula do professor: \n");
     scanf("%d", &prof->matricula);
 
     printf("Digite o nome do professor: \n");
-    // fflush(stdin);
-    // fgets(prof->nome, 50, stdin);
+    getchar(); // Para consumir a nova linha pendente no buffer
+    fgets(prof->nome, 50, stdin);
 
     printf("Digite o salario do professor: \n");
     scanf("%f", &prof->salario);
@@ -26,7 +25,7 @@ void leitura(professor *prof, int *numProf)
     printf("Digite o telefone do professor: \n");
     scanf("%d", &prof->telefone);
 
-    numProf++;
+    (*numProf)++;
 }
 
 void maxSalario(professor *prof, int *numProf)
@@ -36,7 +35,7 @@ void maxSalario(professor *prof, int *numProf)
     for (i = 0; i < *numProf; i++)
     {
         if (prof[i].salario > maior)
-            prof[i].salario = maior;
+            maior = prof[i].salario;
     }
     printf("O maior salario eanh: %.2f\n", maior);
 }
@@ -47,7 +46,7 @@ int exibirDados(professor *prof, int *numProf)
     for (i = 0; i < *numProf; i++)
     {
         printf("Matricula: %d\n", prof[i].matricula);
-        printf("Nome: %s\n", prof[i].nome);
+        printf("Nome: %s", prof[i].nome);
         printf("Salario: %.2f\n", prof[i].salario);
         printf("Telefone: %d\n", prof[i].telefone);
         printf("\n");
@@ -58,34 +57,33 @@ int exibirDados(professor *prof, int *numProf)
 int main(void)
 {
     int numProf = 0;
-    professor prof[20];
+    professor prof[MAX];
     int valor = -1;
 
-    while (0 == 0)
+    while (1)
     {
-        printf("1. Cadastras professor\n2. Exibir maior salario\n3. Imprimir lista "
-               "de professores\n4. Sair\n");
+        printf("1. Cadastrar professor\n2. Exibir maior salario\n3. Imprimir lista de professores\n4. Sair\n");
         scanf("%d", &valor);
 
         switch (valor)
         {
         case 1:
             leitura(&prof[numProf], &numProf);
-            continue;
+            break;
         case 2:
             maxSalario(prof, &numProf);
-            continue;
+            break;
         case 3:
             exibirDados(prof, &numProf);
-            continue;
-        case 4:
             break;
+        case 4:
+            return 0;
 
         default:
             printf("Valor invalido\n");
-            continue;
+            break;
         }
-
-        return 0;
     }
+
+    return 0;
 }
