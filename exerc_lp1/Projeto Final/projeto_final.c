78ef1601e5fc4f17ff39e9a2c4e3d92c6215d1ca
Breno Santana <contatobrenosantana@outlook.com>
2024-03-24 14:25:46 -0300

    Study

---
+++ exerc_lp1/Projeto Final/projeto_final.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Voce acordou em uma sala, você ve uma mesa em sua frente com algo em cima e uma porta de ferro a sua direita\n");
+
+    return 0;
+}
