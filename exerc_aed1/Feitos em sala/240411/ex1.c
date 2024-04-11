15a612e7e4070338339c4707f375918f4ddf1343
BrenoASantana <contatobrenosantana@outlook.com>
2024-04-11 16:25:20 -0300

    Exercício

---
+++ exerc_aed1/Feitos em sala/240411/ex1.c
@@ -0,0 +1,8 @@
+//Dada uma pilha contendo numero inteiros quaisquer,
+//construir uma funcao que coloca os pares na base da pilha e os impares no topo da pilha. 
+//Voce pode usar estruturas auxiliares
+
+int main(){
+
+    return 0;
+}
