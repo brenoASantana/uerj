4bc9670dfbff269128a1cb2ccc03665d43d75ee5
BrenoASantana <contatobrenosantana@outlook.com>
2024-04-29 15:53:19 -0300

    Exercicios Lista 3

--- exerc_aed1/Lista de Exercícios 3/ex1.c
+++ exerc_aed1/Lista de Exercícios 3/ex1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#define MAX 10
+#define MAX 20
 
-int buscaSeq(int *chave, int *vetor)
+int buscaSeq(int chave, int vetor[])
 {
     int i;
     for (i = 0; i < MAX; i++)
@@ -16,12 +16,12 @@ int buscaSeq(int *chave, int *vetor)
 int main()
 {
     int chave;
-    int vetor[MAX] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int vetor[MAX] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,12,13,14,15,16,17,18,19,20};
 
     printf("Informe o valor a ser procurado:");
     scanf("%d", &chave);
 
-    int result = buscaSeq(&chave, &vetor);
+    int result = buscaSeq(chave, vetor);
 
     if (result == -1)
         printf("\n Valor nao encontrado.");
--- exerc_aed1/Lista de Exercícios 3/ex3.c
+++ exerc_aed1/Lista de Exercícios 3/ex3.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#define MAX 20
 
+int buscaBin(int chave, int vetor[])
+{
+    int i;
+    for (i = 0; i < MAX; i++)
+    {
+        if(vetor[i] < chave){
+        if (vetor[i] == chave)
+            return i;
+            }
+    }
+    // Se nao achar, retorna -1 que representa que o valor nao esta no vetor
+    return -1;
+}
+
+int main()
+{
+    int chave;
+    int vetor[MAX] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,12,13,14,15,16,17,18,19,20};
+
+    printf("Informe o valor a ser procurado:");
+    scanf("%d", &chave);
+
+    int result = buscaSeq(chave, vetor);
+
+    if (result == -1)
+        printf("\n Valor nao encontrado.");
+    else
+        printf("Valor na posicao %d", result + 1);
+
+    return 0;
+}
