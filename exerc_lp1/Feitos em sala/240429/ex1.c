d4e5fef8656d47499da718a269058fe7b70d84f0
brenoasantana <contatobrenosantana@outlook.com>
2024-05-06 17:28:05 -0300

    Arquivo certo

--- exerc_lp1/Feitos em sala/240429/ex1.c
+++ exerc_lp1/Feitos em sala/240429/ex1.c
@@ -1,24 +1,27 @@
 // Ler o conteudo de um arquivo de texto e exibi lo na tela
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
-    char texto[100];
+    // Abrindo o arquivo para leitura
     FILE *arq;
     arq = fopen("ex1.txt", "r");
 
+    // Verificando se o arquivo foi aberto corretamente
     if (arq == NULL) {
-        printf("Erro ao abrir o arquivo.\n");
-        return 1; // Exit with error
+        printf("Erro ao abrir o arquivo para leitura.\n");
+        return 1; // Sair com erro
     }
 
-    if (fgets(texto, 100, arq) != NULL) {
-        printf("%s", texto);
-    } else {
-        printf("Erro ao ler o arquivo.\n");
+    // Lendo e imprimindo o conteúdo do arquivo linha por linha
+    char linha[100];
+    while (fgets(linha, sizeof(linha), arq) != NULL) {
+        printf("%s", linha);
     }
 
+    // Fechando o arquivo após a leitura
     fclose(arq);
 
     return 0;
 }
+
+
