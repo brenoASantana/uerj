890407519a5bd3865ccd1618448b02e902894abf
brenoasantana <contatobrenosantana@outlook.com>
2024-05-06 16:14:31 -0300

    a

--- exerc_aed1/Lista de Exercícios 4/ex3.c
+++ exerc_aed1/Lista de Exercícios 4/ex3.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+typedef struct Lista
+{
+    int vetor[15];
+    int quant;
 
-int main(){
+} Lista;
+
+void iniciarLista(Lista *lista)
+{
+    lista->quant = 0;
+}
+
+
+void inserir1Elem(Lista *lista, int valor){
+    lista->vetor[0] = valor;
+}
+
+void inserirUltElem(Lista *lista, int valor){
+    lista->vetor[14] = valor;
+}
+
+int main()
+{
+    Lista lista;
+    iniciarLista(&lista);
+    inserir1Elem(&lista, 1);
+    inserirUltElem(&lista, 14);
 
     return 0;
 }
