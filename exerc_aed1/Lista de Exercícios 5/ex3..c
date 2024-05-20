c4e4ef15a4838a2f1af69c24a4b1a376da3308a4
Breno Santana <contatobrenosantana@outlook.com>
2024-05-19 22:16:35 -0300

    a

---
+++ exerc_aed1/Lista de Exercícios 5/ex3..c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+typedef struct no
+{
+    int info;
+    struct no *prox;
+} no;
+
+int main()
+{
+
+    return 0;
+}
---
+++ exerc_lp1/Feitos em sala/240517/ex.c
@@ -0,0 +1,17 @@
+// Gestao de contatos
+// a)Crie uma struct Contato p/ representar informacoes de um contato: nome, telefome e email
+// b)Permita ao usuario inserir, editar, visualizar e excluir contatos
+// c)Utilize realloc p/ lidar com quantidade variavel de contatos
+// d)Armazene os contatos em ARQUIVO passando por argumentos(ARGC e ARGV)
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct Contato
+{
+
+} Contato;
+
+int main()
+{
+
+    return 0;
+}
