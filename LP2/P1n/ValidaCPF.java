1cdab872e580f511fc25f10ce46941e6c1fdc5e7
Breno Santana <contatobrenosantana@outlook.com>
2024-10-30 18:57:13 -0300

    Validade CPF

--- LP2/P1n/ValidaCPF.java
+++ LP2/P1n/ValidaCPF.java
@@ -4,7 +4,7 @@ import java.util.InputMismatchException;
 public class ValidaCPF {
 
     public static boolean isCPF(String CPF) {
-        // considera-se erro CPF"s formados por uma sequencia de numeros iguais
+        // considera-se erro CPFs formados por uma sequência de números iguais
         if (CPF.equals("00000000000") ||
             CPF.equals("11111111111") ||
             CPF.equals("22222222222") || CPF.equals("33333333333") ||
@@ -17,60 +17,58 @@ public class ValidaCPF {
         char dig10, dig11;
         int sm, i, r, num, peso;
 
-        // "try" - protege o codigo para eventuais erros de conversao de tipo (int)
+        // Protege o código para eventuais erros de conversão de tipo (int)
         try {
-        // Calculo do 1o. Digito Verificador
+            // Cálculo do 1º Dígito Verificador
             sm = 0;
             peso = 10;
-            for (i=0; i<9; i++) {
-        // converte o i-esimo caractere do CPF em um numero:
-        // por exemplo, transforma o caractere "0" no inteiro 0
-        // (48 eh a posicao de "0" na tabela ASCII)
-            num = (int)(CPF.charAt(i) - 48);
-            sm = sm + (num * peso);
-            peso = peso - 1;
+            for (i = 0; i < 9; i++) {
+                num = (int)(CPF.charAt(i) - 48);
+                sm = sm + (num * peso);
+                peso = peso - 1;
             }
 
             r = 11 - (sm % 11);
             if ((r == 10) || (r == 11))
-                dig10 = "0";
-            else dig10 = (char)(r + 48); // converte no respectivo caractere numerico
+                dig10 = '0'; // Mudança de "0" para '0'
+            else dig10 = (char)(r + 48);
 
-        // Calculo do 2o. Digito Verificador
+            // Cálculo do 2º Dígito Verificador
             sm = 0;
             peso = 11;
-            for(i=0; i<10; i++) {
-            num = (int)(CPF.charAt(i) - 48);
-            sm = sm + (num * peso);
-            peso = peso - 1;
+            for (i = 0; i < 10; i++) {
+                num = (int)(CPF.charAt(i) - 48);
+                sm = sm + (num * peso);
+                peso = peso - 1;
             }
 
             r = 11 - (sm % 11);
             if ((r == 10) || (r == 11))
-                 dig11 = "0";
+                dig11 = '0'; // Mudança de "0" para '0'
             else dig11 = (char)(r + 48);
 
-        // Verifica se os digitos calculados conferem com os digitos informados.
+            // Verifica se os dígitos calculados conferem com os dígitos informados.
             if ((dig10 == CPF.charAt(9)) && (dig11 == CPF.charAt(10)))
-                 return(true);
-            else return(false);
-                } catch (InputMismatchException erro) {
-                return(false);
-            }
+                return true;
+            else
+                return false;
+        } catch (InputMismatchException erro) {
+            return false;
         }
+    }
 
-        public static String imprimeCPF(String CPF) {
-            return(CPF.substring(0, 3) + "." + CPF.substring(3, 6) + "." +
-            CPF.substring(6, 9) + "-" + CPF.substring(9, 11));
-        }
+    public static String imprimeCPF(String CPF) {
+        return (CPF.substring(0, 3) + "." + CPF.substring(3, 6) + "." +
+                CPF.substring(6, 9) + "-" + CPF.substring(9, 11));
+    }
 
-		public static String formataCPF(String cpf) {
-			cpf = cpf.replaceAll("[^0-9]", "");
-			return cpf.replaceFirst("(\\d{3})(\\d{3})(\\d{3})(\\d{2})", "$1.$2.$3-$4");
-		}
-	
-		public static long toLong(String cpf) {
-			cpf = cpf.replaceAll("[^0-9]", "");
-			return Long.parseLong(cpf);
-		}
+    public static String formataCPF(String cpf) {
+        cpf = cpf.replaceAll("[^0-9]", "");
+        return cpf.replaceFirst("(\\d{3})(\\d{3})(\\d{3})(\\d{2})", "$1.$2.$3-$4");
+    }
+
+    public static long toLong(String cpf) {
+        cpf = cpf.replaceAll("[^0-9]", "");
+        return Long.parseLong(cpf);
+    }
 }
