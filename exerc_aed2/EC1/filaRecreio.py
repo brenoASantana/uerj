ceb860d99b19721af34fdf817eb1f086b654edff
breno.santana <breno.santana@g.globo>
2025-09-13 21:09:46 -0300

    featconha

--- exerc_aed2/EC1/filaRecreio.py
+++ exerc_aed2/EC1/filaRecreio.py
@@ -1,1 +1,18 @@
-# Problema 1548 beecrowd
+# Problema 1548 - Fila do Recreio
+
+qtd_testes = int(input())
+
+for _ in range(qtd_testes):
+    qtd_alunos = int(input())
+    notas_original = list(map(int, input().split()))
+    
+    # Ordena em ordem decrescente
+    notas_ordenado = sorted(notas_original, reverse=True)
+    
+    # Conta quantos ficaram na mesma posição
+    perm = 0
+    for i in range(qtd_alunos):
+        if notas_original[i] == notas_ordenado[i]:
+            perm += 1
+    
+    print(perm)
